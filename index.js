e7ef139952547331a0804ac0e21193447542221a
rishitha6274 <rishithalattupally01@gmail.com>
2025-06-06 11:11:48 +0530

    updated

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+app.use(cors()); 
+app.use(express.json());
+
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  
